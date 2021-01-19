641e47ba01cdfb8b82931a8f3ef0fa7f95c5db42
wroyce <40677919+wroyce@users.noreply.github.com>
2021-01-19 10:03:17 -0800

    Updated cities.java
    Conflict is fun.

--- Cities.java
+++ Cities.java
@@ -2,5 +2,6 @@ public class Cities {
     public static void main(String[] args){
         System.out.println("Chelsey, Seoul");
         System.out.println("Fernando, Vancouver");
+        System.out.println("William, Italy");
     }
 }
